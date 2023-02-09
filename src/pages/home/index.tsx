1abaacd87b9367c50076a3f61ed532aa9dc9b1c5
unknown <paul933@hotmail.fr>
2023-02-09 08:30:35 +0100

    initialize project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './home.module.scss'
+import Image from 'next/image'
+import marioImage from 'assets/images/marioMovie.jpg'
+
+const home = () => {
+  return (
+    <div className={styles.container}>
+
+        <div className={styles.playerSection}>
+          
+        </div>
+    </div>
+  )
+}
+
+export default home
