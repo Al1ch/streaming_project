840880e4db0fbfff7ab36c2da9001ca2f38cf2b3
Alain <alain.chea@efrei.net>
2023-02-09 19:57:05 +0100

    Revert "fix(card) : card organisation section"
    This reverts commit 0b4f0b6f14714a4c6cf6e12a4e922b3feab4438b.

--- models.types.d.ts
+++
@@ -1,9 +0,0 @@
-import { StaticImageData } from "next/image";
-
-export type Film = {
-    id: number;
-    title: string;
-    author: string[];
-    cardImage: string | StaticImageData;
-    backgroundImage: string | StaticImageData;
-}
